b6336e3f08ddfdf2cc5eaa009bb1ced8c8dccc68
TornadoKsA <52514451+TornadoKsA@users.noreply.github.com>
2019-07-21 17:38:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var prefix = "+";
 var adminprefix = '#'
 
 
-const developers = ["358966532308729863","358966532308729863"]
+const developers = ["358966532308729863"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
