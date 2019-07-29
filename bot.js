23fc321b53055a93c418ecbfd3d7828f81af3844
TornadoKsA <52514451+TornadoKsA@users.noreply.github.com>
2019-07-29 20:02:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`اسم سيرفرك`,"http://twitch.tv/S-F")
+client.user.setGame(`M5ALF / مخالف`,"http://twitch.tv/S-F")
 client.user.setStatus("M5ALF / مخالف")
 });
 
