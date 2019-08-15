e76f4d171349b3f26806e3db18a9dbe209fd0e42
TornadoKsA <52514451+TornadoKsA@users.noreply.github.com>
2019-08-15 20:44:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,28 +1,31 @@
-// Start the bot
 const Discord = require("discord.js");
-const Client = new Discord.Client();
+const client = new Discord.Client();
+var prefix = "!";
+client.on("message", message => {
 
-// Bot Token
-Client.login('BotToken');
-
-// BOT LOG
-
-Client.on('ready',  () => {
-  console.log('=================');
-  console.log('AUTHOR : SAFA7,PATRIC');
-  console.log('=================');
-  console.log('logged in as * [ " ${client.user.username} " ] SERVERS! [ " ${client.guilds.size');
-  console.log('=================');
-  console.log('Bot is on!');
-  console.log('=================');
-Client.user.setStatus("dnd");
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
 });
 
-// Playing
-Client.on('ready', () => {
-Client.user.setGame(`Dragon ✨ | !help`,'https://www.twitch.tv/tariksv');
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Dragon ✨ | !help `,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
 });
 
 
 client.login(process.env.BOT_TOKEN);
-
